0ff2dcfb97534c832f6f6896cfa07b67eee1b320
Monir <mrmonir0558@gmail.com>
2024-08-17 20:12:38 +0600

    Addeded Navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ['./**/*.{html,js}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: '#363958',
+                secondary: '#3E3E3E',
+                yellow: '#FABE4C',
+                background: '#FFFBF0'
+            }
+        }
     },
     plugins: []
 };
